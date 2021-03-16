8d683f4e4ffbfd009e81f0e9f049e44a2fc8f790
GLAZUNOV ALEKSEI <993468@gmail.com>
2021-03-16 10:25:15 +0400

    add prettier and eslint

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Layout() {
+  return <div>123</div>;
+}
+
+export default Layout;
---
+++ src/components/MainPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../Layout";
+import MenuHeader from "../MenuHeader";
+
+function MainPage() {
+  return (
+    <>
+      <MenuHeader />
+      <Layout />
+    </>
+  );
+}
+
+export default MainPage;
