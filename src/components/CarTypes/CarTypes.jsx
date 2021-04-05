3d57804dabfe3819331ee0af208cd48cfdf5fe1c
GLAZUNOV ALEKSEI <993468@gmail.com>
2021-04-05 10:07:44 +0400

    fix

--- src/components/CarTypes/CarTypes.jsx
+++ src/components/CarTypes/CarTypes.jsx
@@ -5,11 +5,7 @@ import style from './car-type.module.scss';
 function CarTypes() {
   return (
     <section className={style.type}>
-      <fieldset
-        id="group1"
-        className={style.group}
-        onChange={(event) => console.log('group1 :>> ', event.target.value)}
-      >
+      <fieldset id="group1" className={style.group}>
         <label htmlFor="input">
           <input type="radio" name="group1" value="Все" defaultChecked />
           <span>Все</span>
